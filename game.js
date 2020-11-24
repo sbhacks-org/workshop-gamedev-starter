1892fc46f08a996908aea2da17db7a59d1d8003e
Jared Flores <jared_flores@cs.ucsb.edu>
2020-11-23 20:28:33 -0800

    End of workshop code

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 function initializeGame() {
 
+    // Reset the game when a player loses or restarts
     function resetGame() {
         this.player.body.setAllowGravity(true);
         this.player.setX(0);
@@ -16,20 +17,24 @@ function initializeGame() {
         this.replayText.setVisible(false);
     }
 
+    // Update the score when a player collects a coin
     function collectCoin(player, coin) {
         coin.disableBody(true, true);
         ++(this.score);
         this.scoreText.setText("Coins: " + this.score.toString() + " / " + this.maxCoins.toString());
     }
 
+    // Reset the game when a player hits a spike
     function hitSpike(player, spike) {
         this.resetGame();
     }
 
+    // Reset the game when a player hits lave
     function hitLava(player, lava) {
         this.resetGame();
     }
 
+    // Reset the game when a player reaches the goal
     function reachGoal(player, goal) {
         this.winText.setVisible(true);
         this.winCoinText.setText("Coins: " + this.score.toString() + " of "  + this.maxCoins.toString() + " coins collected!");
@@ -45,83 +50,64 @@ function initializeGame() {
         this.load.image("goal", "assets/goal.png");
         this.load.image("lava", "assets/lava.png");
         this.load.image("spike", "assets/spike.png");
+
+        // TODO:
+        // Change map_example.json to a map of your choice
         this.load.tilemapTiledJSON("map", "maps/map_1.json");
     }
 
+    function parseObjects(group, name, x, y, offsetX, offsetY, spriteName) {
+        var mapObjects = this.map.getObjectLayer(name)["objects"];
+        var mapObject;
+        for(var i = 0; i < mapObjects.length; ++i) {
+            mapObject = group.create(mapObjects[i].x, mapObjects[i].y - mapObjects[i].height, spriteName).setOrigin(0, 0);
+            if(name != "Lava") mapObject.body.setSize(mapObject.width - x, mapObject.height - y).setOffset(offsetX, offsetY);
+            if(name == "Coins") ++(this.maxCoins);
+        }
+    }
+
     function create() {
+        // Add all collision handlers to the game object
         this.resetGame = resetGame;
         this.collectCoin = collectCoin;
         this.hitSpike = hitSpike;
         this.hitLava = hitLava;
         this.reachGoal = reachGoal;
 
-        var map = this.make.tilemap({ key: "map" });
-        var tileset = map.addTilesetImage("tiles", "tiles");
+        this.parseObjects = parseObjects;
 
-        var platforms = map.createStaticLayer("Platforms", tileset);
-        platforms.setCollisionByExclusion(-1, true);
+        // Parse the map
+        this.map = this.make.tilemap({ key: "map" });
+        var tileset = this.map.addTilesetImage("tiles", "tiles");
 
-        var spikes = this.physics.add.group({
-            allowGravity: false,
-            immovable: true
-        });
+        this.platforms = this.map.createStaticLayer("Platforms", tileset);
+        this.platforms.setCollisionByExclusion(-1, true);
 
-        var spikeObjects = map.getObjectLayer("Spikes")["objects"];
-        var spikeObject;
-        for(var i = 0; i < spikeObjects.length; ++i) {
-            spikeObject = spikes.create(spikeObjects[i].x, spikeObjects[i].y - spikeObjects[i].height, "spike").setOrigin(0, 0);
-            spikeObject.body.setSize(spikeObject.width, spikeObject.height - 8).setOffset(0, 8);
-        }
-
-        var lava = this.physics.add.group({
-            allowGravity: false,
-            immovable: true
-        });
-
-        var lavaObjects = map.getObjectLayer("Lava")["objects"];
-        var lavaObject;
-        for(var i = 0; i < lavaObjects.length; ++i) {
-            lavaObject = lava.create(lavaObjects[i].x, lavaObjects[i].y - lavaObjects[i].height, "lava").setOrigin(0, 0);
-        }
-
-        this.coins = this.physics.add.group({
-            allowGravity: false,
-            immovable: true
-        });
+        // Add the different categories in-game objects
+        this.spikes = this.physics.add.group({ allowGravity: false, immovable: true });
+        this.lava = this.physics.add.group({ allowGravity: false, immovable: true });
+        this.coins = this.physics.add.group({ allowGravity: false, immovable: true });
+        this.goals = this.physics.add.group({ allowGravity: false, immovable: true });
 
         this.maxCoins = 0;
-        var coinObjects = map.getObjectLayer("Coins")["objects"];
-        var coinObject;
-        for(var i = 0; i < coinObjects.length; ++i) {
-            coinObject = this.coins.create(coinObjects[i].x, coinObjects[i].y - coinObjects[i].height, "coin").setOrigin(0, 0);
-            coinObject.body.setSize(coinObject.width - 8, coinObject.height - 7).setOffset(4, 4);
-            ++(this.maxCoins);
+
+        // Add all of the tiled objects
+        this.parseObjects(this.spikes, "Spikes", 0, 8, 0, 8, "spike");
+        this.parseObjects(this.lava, "Lava", 0, 0, 0, 0, "lava");
+        this.parseObjects(this.coins, "Coins", 8, 7, 4, 4, "coin");
+        this.parseObjects(this.goals, "Goals", 15, 0, 8, 0, "goal");
+
+        // Calculate a good starting position (highest platform at x = 0)
+        for(this.startY = 0; this.startY < this.map.height; ++(this.startY)) {
+            if(this.map.getTileAt(0, this.startY, true).index != -1) break;
         }
+        this.startY = (this.startY - 1) * this.map.tileHeight;
 
-        var goals = this.physics.add.group({
-            allowGravity: false,
-            immovable: true
-        });
-
-        var goalObjects = map.getObjectLayer("Goals")["objects"];
-        var goalObject;
-        for(var i = 0; i < goalObjects.length; ++i) {
-            goalObject = goals.create(goalObjects[i].x, goalObjects[i].y - goalObjects[i].height, "goal").setOrigin(0, 0);
-            goalObject.body.setSize(spikeObject.width - 15, spikeObject.height).setOffset(8, 0);
-        }
-
-        var y;
-        for(y = 0; y < map.height; ++y) {
-            if(map.getTileAt(0, y, true).index != -1) {
-                break;
-            }
-        }
-
-        this.startY = (y - 1) * map.tileHeight;
-
+        // Add the player to the game
         this.player = this.physics.add.sprite(0, this.startY, "frog");
         this.player.setCollideWorldBounds(true);
 
+        // Create an animation from the spritesheet
         this.anims.create({
             key: "moving",
             frames: this.anims.generateFrameNumbers("frog", { start: 0, end: 2 }),
@@ -129,40 +115,46 @@ function initializeGame() {
             repeat: -1
         });
 
-        this.physics.add.collider(this.player, platforms);
-        this.physics.add.collider(this.player, spikes, this.hitSpike, null, this);
-        this.physics.add.collider(this.player, lava, this.hitLava, null, this);
-        this.physics.add.overlap(this.player, this.coins, this.collectCoin, null, this);
-        this.physics.add.overlap(this.player, goals, this.reachGoal, null, this);
-        this.physics.world.setBounds(0, 0, map.width * map.tileWidth, map.height * map.tileHeight);
+        // TODO:
+        this.physics.add.collider(this.player, this.platforms);
+        // Add player collision with platforms
+        // Add player collision with spikes and call hitSpike()
+        this.physics.add.collider(this.player, this.spikes, hitSpike, null, this);
+        // Add player collision with lava and call hitLava()
+        this.physics.add.collider(this.player, this.lava, hitLava, null, this);
+        // Add player overlap with coins and call collectCoin()
+        this.physics.add.overlap(this.player, this.coins, collectCoin, null, this);
+        // Add player overlap with goals and call reachGoal()
+        this.physics.add.overlap(this.player, this.goals, reachGoal, null, this);
+        this.physics.world.setBounds(0, 0, this.map.width * this.map.tileWidth, this.map.height * this.map.tileHeight);
 
         this.cursors = this.input.keyboard.createCursorKeys();
         this.cameras.main.startFollow(this.player);
-        this.score = 0;
-        this.won = false;
+
+        // Add the text for coins collected
         this.scoreText = this.add.text(8, 8, "Coins: 0 / " + this.maxCoins.toString(), { font: "12px Arial", fill: "#ffffff" });
         this.scoreText.setScrollFactor(0);
 
         var screenCenterX = this.cameras.main.width >> 1;
         var screenCenterY = this.cameras.main.height >> 1;
+
+        // Create win screen text
         this.winText = this.add.text(screenCenterX, screenCenterY - 30, "You Won!", { font: "48px Arial", fill: "#ffffff", align: "center" });
-        this.winText.setOrigin(0.5, 0.5);
-        this.winText.setScrollFactor(0);
-        this.winText.setVisible(false);
+        this.winText.setOrigin(0.5, 0.5).setScrollFactor(0).setVisible(false);
 
         this.winCoinText = this.add.text(screenCenterX, screenCenterY + 10, "0 of " + this.maxCoins.toString() + " coins collected!", { font: "16px Arial", fill: "#ffffff", align: "center" });
-        this.winCoinText.setOrigin(0.5, 0.5);
-        this.winCoinText.setScrollFactor(0);
-        this.winCoinText.setVisible(false);
+        this.winCoinText.setOrigin(0.5, 0.5).setScrollFactor(0).setVisible(false);
 
         this.replayText = this.add.text(screenCenterX, screenCenterY + 35, "Press space to play again...", { font: "18px Arial", fill: "#ffffff", align: "center" });
-        this.replayText.setOrigin(0.5, 0.5);
-        this.replayText.setScrollFactor(0);
-        this.replayText.setVisible(false);
+        this.replayText.setOrigin(0.5, 0.5).setScrollFactor(0).setVisible(false);
+
+        // Reset the score & win variables
+        this.score = 0;
+        this.won = false;
     }
 
     function update() {
-        if(this.won) {
+        if(this.won) { // If the player has won, keep them still until they press space
             this.player.setVelocityX(0);
             this.player.setVelocityY(0);
             this.player.body.setAllowGravity(false);
@@ -170,11 +162,17 @@ function initializeGame() {
                 this.resetGame();
             }
         } else {
+            // TODO:
+            // Add player animation "moving"
             this.player.anims.play("moving", true);
+            // Set player velocity to 250 in +x direction
             this.player.setVelocityX(250);
-            if(this.cursors.space.isDown && this.player.body.blocked.down) {
+            // Check if the up arrow key is down and if player is touching the ground
+            if(this.cursors.up.isDown && this.player.body.blocked.down) {
                 this.player.setVelocityY(-470);
             }
+            // Set player velocity to 470 in -y direction if yes
+            // Check if a player runs into the wall, then reset the game
             if(this.player.body.blocked.right) {
                 this.resetGame();
             }
